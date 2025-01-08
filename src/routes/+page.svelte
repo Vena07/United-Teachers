6fdb1376c675e624eb893433879e993ddae4a417
KR66666 <kamila.ryhan@gmail.com>
2025-01-08 15:42:41 +0100

    zacalidelatheader

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+
+</script>
+
 <a href="/login">login</a>
 <a href="/register">register</a>
-<a href="/login/forgot-password">reset hesla</a>
+<a href="/asd">reset hesla</a>
 <a href="/profile">profil</a>
 <a href="/posts">Příspěvky</a>
